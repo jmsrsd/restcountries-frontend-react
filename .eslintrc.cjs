a1b6ec698b5ec4baaf3b9d517b530a167a25c690
Jemal Rashidi <jmsrsd@gmail.com>
2023-08-23 23:10:15 +0700

    successfully integrating with react-query to handle data fetching

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -2,22 +2,19 @@ module.exports = {
   root: true,
   env: { browser: true, es2020: true },
   extends: [
-    'eslint:recommended',
-    'plugin:@typescript-eslint/recommended',
-    'plugin:react-hooks/recommended',
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:react-hooks/recommended",
   ],
-  ignorePatterns: ['dist', '.eslintrc.cjs'],
-  parser: '@typescript-eslint/parser',
-  plugins: [
-    'react-refresh',
-    '@tanstack/query',
-  ],
+  ignorePatterns: ["dist", ".eslintrc.cjs"],
+  parser: "@typescript-eslint/parser",
+  plugins: ["react-refresh", "@tanstack/query"],
   rules: {
     "@tanstack/query/exhaustive-deps": "warn",
     "@tanstack/query/prefer-query-object-syntax": "warn",
-    'react-refresh/only-export-components': [
-      'warn',
+    "react-refresh/only-export-components": [
+      "warn",
       { allowConstantExport: true },
     ],
   },
-}
+};
--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,35 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useQuery } from "@tanstack/react-query";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const uri = `https://restcountries.com/v3.1/all`;
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+  const { isLoading, error, data } = useQuery({
+    queryKey: [uri],
+    queryFn: async () => {
+      const response = await fetch(uri, {
+        method: "GET",
+        mode: "cors",
+      });
+
+      return await response.json();
+    },
+  });
+
+  if (isLoading) {
+    return <pre>{`Loading...`}</pre>;
+  }
+
+  if (error) {
+    return (
+      <pre>{`Oops.. Something went wrong: ${JSON.stringify(
+        error,
+        null,
+        2,
+      )}`}</pre>
+    );
+  }
+
+  return <pre>{JSON.stringify(data, null, 2)}</pre>;
 }
 
-export default App
+export default App;
